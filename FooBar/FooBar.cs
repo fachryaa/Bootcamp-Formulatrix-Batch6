71cceb7d96e8d7af109d9715f2e078c428b17728
fachryaa <fajri.fr85@gmail.com>
2023-10-25 14:39:34 +0700

    Add: Condition FizzBuzz

--- FooBar/FooBar.cs
+++ FooBar/FooBar.cs
@@ -3,15 +3,52 @@ namespace FooBar;
 
 public static class FooBar
 {
+	private static Dictionary<int, string> _conditions = new Dictionary<int, string>()
+	{
+		{3, "Foo"},
+		{5, "Bar"}
+	};
+	
+	public static void AddCondition(int key, string value)
+	{
+		// check if key already exists
+		if (_conditions.ContainsKey(key))
+		{
+			_conditions[key] = value;
+		}
+		else
+		{
+			_conditions.Add(key,value);
+		}
+	}
+	
+	public static StringBuilder Decide(int num)
+	{
+		// create StringBuilder
+		StringBuilder result = new();
+		
+		// return 0 if num=0
+		if (num == 0) return new StringBuilder("0");
+		
+		// check all condition using foreach
+		int counter = 0; // counter to check at least one condition is true
+		foreach (var condition in _conditions)
+		{
+			// if condition is true, append the value
+			if (num % condition.Key == 0)
+			{
+				result.Append(condition.Value);
+				counter++;
+			}
+		}
+		
+		// if no condition is true, return num
+		if (counter == 0) result.Append(num);		
+		
+		return result;
+	}
 	private static StringBuilder _result = new StringBuilder();
 
-	private static StringBuilder Decide(int num)
-	{
-		if (num % 3 == 0 && num % 5 == 0 && num != 0) return new StringBuilder("FooBar");
-		else if (num % 3 == 0 && num != 0) return new StringBuilder("Foo");
-		else if (num % 5 == 0 && num != 0) return new StringBuilder("Bar");
-		else return new StringBuilder(num.ToString());
-	}
 	
 	public static string Run(int num)
 	{
--- FooBar/Program.cs
+++ FooBar/Program.cs
@@ -3,15 +3,25 @@
 static class Program
 {
 	static void Main(string[] Args)
-	{
-		int num;
-		do 
-		{
-			Console.Write("Masukkan angka: ");
-		} while (!int.TryParse(Console.ReadLine(), out num));
+	{		
+		// Input num
+		InputNum(out int num);		
 		
+		// Add condition
+		FooBar.AddCondition(9, "Fizz");
+		FooBar.AddCondition(11, "Buzz");
+		
+		// Run the library
 		string result = FooBar.Run(num);
 		
 		Console.WriteLine(result);
+	}
+	
+	static void InputNum(out int num)
+	{
+		do 
+		{
+			Console.Write("Masukkan angka: ");
+		} while (!int.TryParse(Console.ReadLine(), out num) || num < 0);
 	}
 }
